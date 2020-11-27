11f44324074269ad0c8743a1d0bf5450a6529d69
vfsc2018 <uconvert@yahoo.com>
2020-11-27 17:16:47 +0700

    send sms

---
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/user/Sms.java
@@ -0,0 +1,32 @@
+package com.salesmanager.shop.model.user;
+
+import java.io.Serializable;
+
+/**
+ * Object containing password information
+ * for change password request
+ * @author carlsamson
+ *
+ */
+public class Sms implements Serializable{
+  
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+  private String text;
+  private String phone;
+  public String getPhone() {
+    return phone;
+  }
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+  public String getText() {
+    return text;
+  }
+  public void setText(String text) {
+    this.text = text;
+  }
+
+}
