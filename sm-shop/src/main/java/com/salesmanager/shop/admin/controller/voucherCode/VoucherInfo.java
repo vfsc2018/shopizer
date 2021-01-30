240c6b6c0350de350781677336dcb487eed16674
vfsc2018 <uconvert@yahoo.com>
2021-01-30 11:51:27 +0700

    new voucher info response

---
+++ sm-shop/src/main/java/com/salesmanager/shop/admin/controller/voucherCode/VoucherInfo.java
@@ -0,0 +1,65 @@
+package com.salesmanager.shop.admin.controller.vouchercode;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class VoucherInfo{
+
+	private String code;
+	private String description;
+	private Integer point;
+	private Integer discount;
+	private Integer percent;
+
+	public VoucherInfo(){
+
+	}
+	
+	public String getCode() {
+		return code;
+	}
+
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getPoint() {
+		return point;
+	}
+
+
+	public void setPoint(Integer point) {
+		this.point = point;
+	}
+
+
+	public Integer getDiscount() {
+		return discount;
+	}
+
+
+	public void setDiscount(Integer discount) {
+		this.discount = discount;
+	}
+
+
+	public Integer getPercent() {
+		return percent;
+	}
+
+
+	public void setPercent(Integer percent) {
+		this.percent = percent;
+	}
+
+	
+}
