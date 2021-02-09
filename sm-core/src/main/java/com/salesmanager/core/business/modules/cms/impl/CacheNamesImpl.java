087dd355e6bb898a14a8593baaab380217745469
vfsc2018 <uconvert@yahoo.com>
2021-02-09 14:18:01 +0700

    add cache serve api

---
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/cms/impl/CacheNamesImpl.java
@@ -0,0 +1,15 @@
+package com.salesmanager.core.business.modules.cms.impl;
+
+
+public class CacheNamesImpl {
+
+  CacheNamesImpl(){}
+
+
+  public final static String CACHE_CUSTOMER_ORDER = "CACHE_CUSTOMER_ORDER";
+  public final static String CACHE_PRODUCT = "CACHE_PRODUCT";
+  public final static String CACHE_MERCHANT = "CACHE_MERCHANT";
+  public final static String CACHE_CONTENT = "CACHE_CONTENT";
+
+}
+
---
+++ sm-shop/src/main/java/com/salesmanager/shop/application/config/MultipleCacheManagerConfig.java
@@ -0,0 +1,69 @@
+package com.salesmanager.shop.application.config;
+
+import com.salesmanager.core.business.modules.cms.impl.CacheNamesImpl;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class MultipleCacheManagerConfig extends CachingConfigurerSupport {
+	
+
+	// @Bean
+	// public net.sf.ehcache.CacheManager ehCacheManager() {
+	// 	CacheConfiguration productGroupCache = new CacheConfiguration();
+	// 	productGroupCache.setName(ProductGroupCacheManagerImpl.NAMED_CACHE);
+	// 	productGroupCache.setMemoryStoreEvictionPolicy("LRU");
+	// 	productGroupCache.setMaxEntriesLocalHeap(1000);
+	// 	productGroupCache.setTimeToLiveSeconds(3600);
+
+	// 	CacheConfiguration productDetailCache = new CacheConfiguration();
+	// 	productDetailCache.setName(CacheNamesImpl.NAMED_CACHE);
+	// 	productDetailCache.setMemoryStoreEvictionPolicy("LRU");
+	// 	productDetailCache.setMaxEntriesLocalHeap(100);
+	// 	productDetailCache.setTimeToLiveSeconds(3600);
+
+	// 	CacheConfiguration customerOrderCache = new CacheConfiguration();
+	// 	customerOrderCache.setName(CacheNamesImpl.NAMED_CACHE);
+	// 	customerOrderCache.setMemoryStoreEvictionPolicy("LRU");
+	// 	customerOrderCache.setMaxEntriesLocalHeap(1000);
+	// 	customerOrderCache.setTimeToLiveSeconds(3600);
+
+	// 	net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
+	// 	return net.sf.ehcache.CacheManager.getInstance();
+	// 	config.addCache(customerOrderCache);
+	// 	config.addCache(productDetailCache);
+	// 	config.addCache(productGroupCache);
+	// 	return net.sf.ehcache.CacheManager.newInstance(config);
+	// }
+	
+	@Bean
+	@Override
+	public CacheManager cacheManager() {
+		net.sf.ehcache.CacheManager caches = net.sf.ehcache.CacheManager.getInstance();
+		caches.addCache(CacheNamesImpl.CACHE_CUSTOMER_ORDER);
+		caches.addCache(CacheNamesImpl.CACHE_MERCHANT);
+		caches.addCache(CacheNamesImpl.CACHE_PRODUCT);
+		caches.addCache(CacheNamesImpl.CACHE_CONTENT);
+		return new EhCacheCacheManager(caches);//net.sf.ehcache.CacheManager.getCacheManager("com.shopizer.OBJECT_CACHE"));
+		// return new ConcurrentMapCacheManager(ProductGroupCacheManagerImpl.NAMED_CACHE);
+
+
+		// CacheManager cache = net.sf.ehcache.CacheManager.getInstance();
+		// return null; //net.sf.ehcache.CacheManager.getInstance();
+		// return cache.getCacheManager();
+		// return new EhCacheCacheManager(ehCacheManager());
+	}
+
+    @Bean
+    public CacheManager alternateCacheManager() {
+		// return new EhCacheCacheManager(net.sf.ehcache.CacheManager.getCacheManager("com.shopizer.OBJECT_CACHE"));
+        return new ConcurrentMapCacheManager();//CacheNamesImpl.NAMED_CACHE,CacheNamesImpl.NAMED_CACHE);
+    }
+}
